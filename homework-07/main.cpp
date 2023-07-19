d70c1b9102295c6f6471e06341f6337ce31fefa6
turbohedgehog <unlinker@mail.ru>
2023-07-19 19:37:49 +0300

    Bulk
    Timus 1227
    
    Timus 1215
    
    Timus 1497

--- homework-07/main.cpp
+++ homework-07/main.cpp
@@ -1,10 +1,7 @@
 
+#include "bulk.h"
+
 #include <iostream>
-#include <cstdlib>
-#include <map>
-#include <functional>
-#include <vector>
-#include <algorithm>
 
 //#define TEST
 
@@ -24,63 +21,13 @@ int main (int argc, char *argv[]) {
   //std::istringstream in("cmd1\ncmd2\ncmd3\ncmd4\ncmd5\nEOF");
   std::istringstream in("cmd1\ncmd2\n{\ncmd3\ncmd4\n}\n{\ncmd5\ncmd6\n{\ncmd7\ncmd8\n}\ncmd9\n}\n{\ncmd10\ncmd11\nEOF");
   std::cin.rdbuf(in.rdbuf());
-#endif
+  #endif
 
-  bool eof = false;
-  std::string cur_command;
-  std::vector<std::string> commands;
-  int block_counter = 0;
+  auto bulk_parser = homework_07::BulkParser(block_size);
+  bulk_parser.AppendBulkExecutor<homework_07::BulkPrinter>();
+  bulk_parser.AppendBulkExecutor<homework_07::BulkFileWriter>();
 
-  auto print_and_clear_commands = [&] {
-    if (commands.empty()) {
-      return;
-    }
-
-    std::cout << "bulk: ";
-    std::cout << *commands.begin();
-    std::for_each(std::next(commands.begin()), commands.end(), [] (auto cmd) {
-      std::cout << ", " << cmd;
-    });
-
-    std::cout << '\n';
-    commands.clear();
-  };
-
-  std::map<std::string, std::function<void()>> command_map = {
-    {"{", [&] {
-      ++block_counter;
-      if (block_counter == 1) {
-        print_and_clear_commands();
-      }
-    }},
-    {"}", [&] {
-      --block_counter;
-      if (block_counter == 0) {
-        print_and_clear_commands();
-      }
-      block_counter = std::max(0, block_counter);
-    }},
-    {"EOF", [&] {
-      if (block_counter == 0) {
-        print_and_clear_commands();
-      }
-
-      eof = true;
-    }},
-  };
-
-  while(!eof) {
-    std::cin >> cur_command;
-    auto command_it = command_map.find(cur_command);
-    if (command_it == command_map.end()) {
-      commands.push_back(cur_command);
-      if (commands.size() >= block_size && block_counter == 0) {
-        print_and_clear_commands();
-      }
-    } else {
-      command_it->second();
-    }
-  }
+  bulk_parser.Run();
 
   return 0;
 }
